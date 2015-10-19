f9ae1b08425014840ff8247a53c4abe1a889fb8c
ungarj <joachim.ungar@gmail.com>
2015-10-19 17:07:59 +0200

    add test script

---
+++ test.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import argparse
+import numpy as np
+from sqlite3 import Binary as sbinary
+
+from utils_geopackage import *
+
+def main(args):
+
+    output_file = "test.gpkgx"
+
+    try:
+        run_tests(output_file)
+    except:
+        raise
+    finally:
+        #os.remove(output_file)
+        pass
+
+def run_tests(output_file):
+
+
+    # Test if file is created.
+    first_geopackage = EOGeopackage(output_file, "xray", 4326)
+    try:
+        assert os.path.isfile(output_file)
+        first_creation_time = os.path.getctime(output_file)
+        print "file creation OK"
+    except:
+        raise
+    try:
+        assert schema_is_ok(first_geopackage)
+        print "schema OK"
+    except:
+        raise
+
+    # Test if file is overwritten.
+    second_geopackage = EOGeopackage(
+        output_file,
+        "image/TIFF",
+        4326,
+        overwrite=True
+        )
+    try:
+        assert os.path.isfile(output_file)
+        second_creation_time = os.path.getctime(output_file)
+    except:
+        raise
+    try:
+        assert second_creation_time > first_creation_time
+        print "file overwrite OK"
+    except:
+        raise
+    try:
+        assert schema_is_ok(second_geopackage)
+        print "schema OK"
+    except:
+        raise
+
+    # Test if file is updated (creation time must stay the same).
+    updated_geopackage = EOGeopackage(output_file, "xray", 4326)
+    try:
+        assert os.path.isfile(output_file)
+        updated_creation_time = os.path.getctime(output_file)
+    except:
+        raise
+    try:
+        assert second_creation_time == updated_creation_time
+        print "file update OK"
+    except:
+        raise
+    try:
+        assert schema_is_ok(updated_geopackage)
+        print "schema OK"
+    except:
+        raise
+
+    # Test insert data.
+    test_data = np.random.rand(256, 256)
+    zoom, row, col = (3, 5, 7)
+    try:
+        updated_geopackage.insert_tile(zoom, row, col, test_data)
+        pass
+    except:
+        raise
+
+    # Read data.
+    try:
+        test_read = updated_geopackage.get_tiledata(zoom, row, col)
+        print type(test_read)
+        assert isinstance(test_read, np.ndarray)
+        assert test_read.all() == test_data.all()
+    except:
+        raise
+
+
+def schema_is_ok(geopackage):
+    """
+    Checks if all necessary tables exist.
+    """
+    with geopackage.db_connection as db_connection:
+        cursor = db_connection.cursor()
+        for table in sql_create_tables:
+            cursor.execute("""
+                SELECT name FROM sqlite_master WHERE type='table' AND name='%s'
+                """ %(table)
+            )
+            if cursor.fetchone():
+                pass
+            else:
+                return table
+    return True
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
