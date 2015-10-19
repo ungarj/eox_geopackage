e1f203f57e82f53616eae23b5152c304a3e9a43c
ungarj <joachim.ungar@gmail.com>
2015-10-19 17:08:29 +0200

    intermediary commit

--- utils_geopackage.py
+++ utils_geopackage.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
-import collections
+# Parts of this tool were taken from https://github.com/GitHubRGI/geopackage-python
+
+from collections import OrderedDict
+from sqlite3 import connect
+import os
+import io
 
 class EOGeopackage():
     """
@@ -11,65 +16,176 @@ class EOGeopackage():
         return self
 
     def __init__(
-        filename,
-        directory,
+        self,
+        file_path,
         data_type,
-        overwrite=False,
-        update=True
+        projection,
+        overwrite=False
         ):
+        """
+        Initializes geopackage file and creates EOGeopackage object.
+        """
         try:
             assert data_type in ("image/TIFF", "xray")
         except:
             raise
-        self.name = filename
-        self.path = os.path.join(directory, filename)
-        self.__db_connection = connect(self.path)
+        self.file_path = file_path
         self.data_type = data_type
+        self.projection = projection
         self.overwrite = overwrite
-        __create_file(overwrite)
+        self.projection, self.overwrite
+        self.db_connection = connect(self.file_path)
+        self.__create_file(overwrite=overwrite)
 
-    def __create_file(overwrite=False, update=False):
-        if not os.path.isfile:
-            # Create new file if it doesn't exist yet.
-            pass
-            # create file
-            # create schema
-        elif overwrite:
-            # If file exists but shall be overwritten, delete file and create
-            # new one.
-            os.remove(self.path)
-            __create_file(self.path)
-        else:
-            # If file exists, check whether it holds the correct schema. Create
-            # schema if necessary and proceed.
-            # check if schema exists
-            # create schema
-            __create_schema(self.path)
 
-    def __create_schema(self.__db_connection):
-        with self.__db_connection as db_connection:
+    def __create_file(self, overwrite=False):
+        """
+        Creates a new geopackage file (i.e. including schema).
+        """
+        try:
+            assert os.path.isfile(self.file_path)
+        except:
+            raise IOError
+        if overwrite:
+            os.remove(self.file_path)
+        self.db_connection = connect(self.file_path, detect_types=sqlite3.PARSE_DECLTYPES)
+        self.__create_schema()
+
+
+    def __create_schema(self):
+        with self.db_connection as db_connection:
             cursor = db_connection.cursor()
-            # Create tables.
-            for table, statement in sql_create_tables.iteritems():
-                cursor.execute(statement)
-            # Create triggers.
-            for trigger, statement in sql_create_triggers.iteritems():
-                cursor.execute(statement)
 
-sql_create_tables = {
-    "gpkg_spatial_ref_sys":
+            # Basic schema.
+            try:
+                for table, statement in sql_create_tables.iteritems():
+                    cursor.execute(statement)
+            except:
+                raise
+
+            # Spatial reference.
+            try:
+                cursor.execute("""
+                    INSERT INTO gpkg_spatial_ref_sys (
+                        srs_id,
+                        organization,
+                        organization_coordsys_id,
+                        srs_name,
+                        definition)
+                    VALUES (4326, ?, 4326, ?, ?)
+                    """,
+                    ("epsg", "WGS 84", ref_sys_wkt[self.projection])
+                )
+            except:
+                print "Warning: EPSG definition already exists"
+
+            # Dataset description.
+            try:
+                cursor.execute("""
+                    INSERT OR IGNORE INTO gpkg_contents (
+                        table_name,
+                        data_type,
+                        identifier,
+                        description,
+                        min_x,
+                        max_x,
+                        min_y,
+                        max_y,
+                        srs_id)
+                    VALUES (?, ?, ?, ?, 0, 0, 0, 0, ?);
+                    """,
+                    (
+                    "tiles",
+                    "tiles",
+                    (self.data_type + " Raster Tiles"),
+                    content_description,
+                    str(self.projection)
+                    )
+                )
+            except:
+                raise
+                pass
+
+
+    def insert_tile(self, zoom, row, col, data):
+        if self.data_type == "xray":
+            print "hui"
+        with self.db_connection as db_connection:
+            cursor = db_connection.cursor()
+            cursor.execute("""
+                INSERT INTO tiles
+                    (zoom_level, tile_row, tile_column, tile_data)
+                    VALUES (?,?,?,?)
+            """, (zoom, row, col, data))
+
+
+    def get_tiledata(self, zoom, row, col):
+        with self.db_connection as db_connection:
+            cursor = db_connection.cursor()
+            cursor.execute("""
+                SELECT tile_data from tiles WHERE
+                zoom_level=? AND tile_row=? AND tile_column=?;
+            """, (str(zoom), str(row), str(col)))
+            return cursor.fetchone()[0]
+
+    def __exit__(self, type, value, traceback):
+        """Resource cleanup on destruction."""
+        self.db_connection.close()
+
+
+# From http://stackoverflow.com/questions/18621513/python-insert-numpy-array-into-sqlite3-database
+def adapt_array(arr):
     """
-    CREATE TABLE gpkg_spatial_ref_sys (
+    http://stackoverflow.com/a/31312102/190597 (SoulNibbler)
+    """
+    out = io.BytesIO()
+    np.save(out, arr)
+    out.seek(0)
+    return sqlite3.Binary(out.read())
+def convert_array(text):
+    out = io.BytesIO(text)
+    out.seek(0)
+    return np.load(out)
+import sqlite3
+import numpy as np
+# Converts np.array to TEXT when inserting
+sqlite3.register_adapter(np.ndarray, adapt_array)
+# Converts TEXT to np.array when selecting
+sqlite3.register_converter("array", convert_array)
+
+
+content_description = """
+Experimental EO GPKG raster tiles by EOX IT Services (ju@eox.at)
+"""
+
+
+ref_sys_wkt = {
+    4326: """
+    GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,
+    298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG",
+    "6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT
+    ["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],
+    AUTHORITY["EPSG","4326"]]
+    """
+    }
+
+
+sql_create_tables = OrderedDict([
+    ("gpkg_spatial_ref_sys",
+    """
+    CREATE TABLE IF NOT EXISTS gpkg_spatial_ref_sys (
       srs_name TEXT NOT NULL,
       srs_id INTEGER NOT NULL PRIMARY KEY,
       organization TEXT NOT NULL,
       organization_coordsys_id INTEGER NOT NULL,
       definition  TEXT NOT NULL,
       description TEXT
-    """,
-    "gpkg_contents":
+    );
     """
-    CREATE TABLE gpkg_contents (
+    ),
+    ("gpkg_contents",
+    """
+    CREATE TABLE IF NOT EXISTS gpkg_contents (
       table_name TEXT NOT NULL PRIMARY KEY,
       data_type TEXT NOT NULL,
       identifier TEXT UNIQUE,
@@ -83,10 +199,11 @@ sql_create_tables = {
       srs_id INTEGER,
       CONSTRAINT fk_gc_r_srs_id FOREIGN KEY (srs_id) REFERENCES
         gpkg_spatial_ref_sys(srs_id)
-    """,
-    "gpkg_geometry_columns":
+    );
+    """),
+    ("gpkg_geometry_columns",
     """
-    CREATE TABLE gpkg_geometry_columns (
+    CREATE TABLE IF NOT EXISTS gpkg_geometry_columns (
       table_name TEXT NOT NULL,
       column_name TEXT NOT NULL,
       geometry_type_name TEXT NOT NULL,
@@ -100,10 +217,10 @@ sql_create_tables = {
       CONSTRAINT fk_gc_srs FOREIGN KEY (srs_id) REFERENCES gpkg_spatial_ref_sys
         (srs_id)
     );
-    """,
-    "gpkg_tile_matrix_set":
+    """),
+    ("gpkg_tile_matrix_set",
     """
-    CREATE TABLE gpkg_tile_matrix_set (
+    CREATE TABLE IF NOT EXISTS gpkg_tile_matrix_set (
       table_name TEXT NOT NULL PRIMARY KEY,
       srs_id INTEGER NOT NULL,
       min_x DOUBLE NOT NULL,
@@ -115,10 +232,10 @@ sql_create_tables = {
       CONSTRAINT fk_gtms_srs FOREIGN KEY (srs_id) REFERENCES
         gpkg_spatial_ref_sys (srs_id)
     );
-    """,
-    "gpkg_tile_matrix":
+    """),
+    ("gpkg_tile_matrix",
     """
-    CREATE TABLE gpkg_tile_matrix (
+    CREATE TABLE IF NOT EXISTS gpkg_tile_matrix (
       table_name TEXT NOT NULL,
       zoom_level INTEGER NOT NULL,
       matrix_width INTEGER NOT NULL,
@@ -131,118 +248,16 @@ sql_create_tables = {
       CONSTRAINT fk_tmm_table_name FOREIGN KEY (table_name) REFERENCES
         gpkg_contents(table_name)
     );
+    """),
+    ("tiles",
     """
-    }.OrderedDict()
-
-sql_create_triggers = """
-    CREATE TRIGGER 'gpkg_tile_matrix_zoom_level_insert'
-    BEFORE INSERT ON 'gpkg_tile_matrix'
-    FOR EACH ROW BEGIN
-    SELECT RAISE(ABORT, 'insert on table ''gpkg_tile_matrix'' violates
-      constraint: zoom_level cannot be less than 0')
-    WHERE (NEW.zoom_level < 0);
-    END
-
-    CREATE TRIGGER 'gpkg_tile_matrix_zoom_level_update'
-    BEFORE UPDATE of zoom_level ON 'gpkg_tile_matrix'
-    FOR EACH ROW BEGIN
-    SELECT RAISE(ABORT, 'update on table ''gpkg_tile_matrix'' violates
-      constraint: zoom_level cannot be less than 0')
-    WHERE (NEW.zoom_level < 0);
-    END
-
-    CREATE TRIGGER 'gpkg_tile_matrix_matrix_width_insert'
-    BEFORE INSERT ON 'gpkg_tile_matrix'
-    FOR EACH ROW BEGIN
-    SELECT RAISE(ABORT, 'insert on table ''gpkg_tile_matrix'' violates
-      constraint: matrix_width cannot be less than 1')
-    WHERE (NEW.matrix_width < 1);
-    END
-
-    CREATE TRIGGER 'gpkg_tile_matrix_matrix_width_update'
-    BEFORE UPDATE OF matrix_width ON 'gpkg_tile_matrix'
-    FOR EACH ROW BEGIN
-    SELECT RAISE(ABORT, 'update on table ''gpkg_tile_matrix'' violates
-      constraint: matrix_width cannot be less than 1')
-    WHERE (NEW.matrix_width < 1);
-    END
-
-    CREATE TRIGGER 'gpkg_tile_matrix_matrix_height_insert'
-    BEFORE INSERT ON 'gpkg_tile_matrix'
-    FOR EACH ROW BEGIN
-    SELECT RAISE(ABORT, 'insert on table ''gpkg_tile_matrix'' violates
-      constraint: matrix_height cannot be less than 1')
-    WHERE (NEW.matrix_height < 1);
-    END
-
-    CREATE TRIGGER 'gpkg_tile_matrix_matrix_height_update'
-    BEFORE UPDATE OF matrix_height ON 'gpkg_tile_matrix'
-    FOR EACH ROW BEGIN
-    SELECT RAISE(ABORT, 'update on table ''gpkg_tile_matrix'' violates
-      constraint: matrix_height cannot be less than 1')
-    WHERE (NEW.matrix_height < 1);
-    END
-
-    CREATE TRIGGER 'gpkg_tile_matrix_pixel_x_size_insert'
-    BEFORE INSERT ON 'gpkg_tile_matrix'
-    FOR EACH ROW BEGIN
-    SELECT RAISE(ABORT, 'insert on table ''gpkg_tile_matrix'' violates
-      constraint: pixel_x_size must be greater than 0')
-    WHERE NOT (NEW.pixel_x_size > 0);
-    END
-
-    CREATE TRIGGER 'gpkg_tile_matrix_pixel_x_size_update'
-    BEFORE UPDATE OF pixel_x_size ON 'gpkg_tile_matrix'
-    FOR EACH ROW BEGIN
-    SELECT RAISE(ABORT, 'update on table ''gpkg_tile_matrix'' violates
-      constraint: pixel_x_size must be greater than 0')
-    WHERE NOT (NEW.pixel_x_size > 0);
-    END
-
-    CREATE TRIGGER 'gpkg_tile_matrix_pixel_y_size_insert'
-    BEFORE INSERT ON 'gpkg_tile_matrix'
-    FOR EACH ROW BEGIN
-    SELECT RAISE(ABORT, 'insert on table ''gpkg_tile_matrix'' violates
-      constraint: pixel_y_size must be greater than 0')
-    WHERE NOT (NEW.pixel_y_size > 0);
-    END
-
-    CREATE TRIGGER 'gpkg_tile_matrix_pixel_y_size_update'
-    BEFORE UPDATE OF pixel_y_size ON 'gpkg_tile_matrix'
-    FOR EACH ROW BEGIN
-    SELECT RAISE(ABORT, 'update on table ''gpkg_tile_matrix'' violates
-      constraint: pixel_y_size must be greater than 0')
-    WHERE NOT (NEW.pixel_y_size > 0);
-    END
-    """
-    # insert
-    # update
-    # .name
-    # .path
-    # get_tile(zoom, row, col)
-    # get_tile_as_array(zoom, row, col)
-
-print help(herbert)
-
-    # if not output_file, create output_file
-    # if output_file, if not eo_gpkg schema, initialize_eo_gpkg_schema
-    # else:
-    # determine highest zoom level
-    # tilify highest zoom level
-    # write zoom level
-    # create pyramids until zoom level 0
-
-
-def create_eo_gpkg_tiles(output_file):
-    # create spatialite file
-    # initialize_eo_gpkg_schema
-    pass
-
-
-def initialize_eo_gpkg_schema(output_file):
-    out_tiles = None
-    # Check if file exists.
-    # Create spatialite file if not.
-
-    # Open existing file
-    pass
+    CREATE TABLE IF NOT EXISTS tiles (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      zoom_level INTEGER NOT NULL,
+      tile_column INTEGER NOT NULL,
+      tile_row INTEGER NOT NULL,
+      tile_data ARRAY NOT NULL,
+      UNIQUE (zoom_level, tile_column, tile_row)
+    );
+    """)
+    ])
